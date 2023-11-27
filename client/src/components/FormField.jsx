2455d74e45b89f0c43e61b97b87fc700f543fcdb
Folka2134 <folka2134@gmail.com>
2023-11-27 17:28:30 +0000

    updated FormField component

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,9 +1,44 @@
 import React from "react";
 
-const FormField = () => {
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => {
   return (
     <div>
-      <span>FormField</span>
+      <div className="mb-2 flex items-center gap-2">
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
+        >
+          {labelName}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className="rounded-[5px] bg-[#ececf1] px-2 py-1 text-xs font-semibold text-black"
+          >
+            Surprise Me
+          </button>
+        )}
+      </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 outline-none focus:border-[#4649ff] focus:ring-[#4649ff]"
+      />
     </div>
   );
 };

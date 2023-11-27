c774dcbe994dee280fc5596df44653bc1f1ff27b
Folka2134 <folka2134@gmail.com>
2023-11-27 17:01:25 +0000

    created home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,61 @@
 import React, { useState, useEffect } from "react";
 
-const Home = () => {
+import { Loader, Card, FormField } from "../components";
+
+const RenderCards = ({ data, title }) => {
+  if (data?.length > 0) {
+    return data.map((post) => <Card key={post.id} {...post} />);
+  }
+
   return (
-    <div>
-      <span>Home</span>
-    </div>
+    <h2 className="mt-5 text-xl font-bold uppercase text-[#6449ff]">{title}</h2>
+  );
+};
+
+const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+  const [searchText, setSearchText] = useState("");
+
+  return (
+    <section className="mx-auto max-w-7xl">
+      <div>
+        <h1 className="text-[32px] font-extrabold text-[#222328]">
+          The Community Showcase
+        </h1>
+        <p className="mt-2 max-w-[500px] text-[16px] text-[#666e75]">
+          Browser through a collection of unique images powered by DALL-E AI
+        </p>
+      </div>
+
+      <div className="mt-16">
+        <FormField />
+      </div>
+
+      <div className="mt-10">
+        {loading ? (
+          <div className="flex items-center justify-center">
+            <Loader />
+          </div>
+        ) : (
+          <>
+            {searchText && (
+              <h2 className="mb-3 text-xl font-medium text-[#666e75]">
+                Showing results for{" "}
+                <span className="text-[#222328]">{searchText}</span>
+              </h2>
+            )}
+            <div className="grid grid-cols-1 gap-3 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
+              {searchText ? (
+                <RenderCards data={[]} title="No search results found" />
+              ) : (
+                <RenderCards data={[]} title="No posts found" />
+              )}
+            </div>
+          </>
+        )}
+      </div>
+    </section>
   );
 };
 
